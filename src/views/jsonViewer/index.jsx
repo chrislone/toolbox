0902d4405f4fabd4527ac935f1f63ceb02e34e50
chrislone <chriislonee@outlook.com>
2024-03-24 22:03:21 +0800

    update

--- src/views/jsonViewer/index.jsx
+++ src/views/jsonViewer/index.jsx
@@ -3,8 +3,6 @@ import { useState } from "react";
 import VanillaJSONEditor from "./vanillaJSONEditor";
 import "./index.css";
 
-const { TextArea } = Input;
-
 function App() {
   const [showEditor] = useState(true);
   const [readOnly] = useState(false);
@@ -30,18 +28,7 @@ function App() {
   return (
     <>
       <Row style={{ margin: "0 auto", width: "1200px" }}>
-        <Col span={12}>
-          <div className={"text-area-wrapper"}>
-            <TextArea
-              placeholder="your json"
-              autoSize={{
-                minRows: 10,
-              }}
-              onChange={onJsonChangeHandler}
-            />
-          </div>
-        </Col>
-        <Col span={12}>
+        <Col span={24}> 
           {showEditor && (
             <div className={"text-area-wrapper"}>
               <VanillaJSONEditor
